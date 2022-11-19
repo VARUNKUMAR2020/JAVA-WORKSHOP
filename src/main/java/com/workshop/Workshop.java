af6189513717106359de22b1479b3fb3f8b10c0c
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-19 08:31:18 +0530

    Add: Workshop on Boat Problem

---
+++ src/main/java/com/workshop/Workshop.java
@@ -0,0 +1,14 @@
+package com.workshop;
+public class Workshop 
+{
+    public static void main( String[] args )
+    { 
+        int people[]=new int[10];
+        System.out.println("Two Member in a boat: 100");
+        System.out.println("One member in the boat: 70");
+        int twoMember =100;
+        int oneMember=70; 
+        int average=(twoMember+oneMember)/3;
+        System.out.println("Average amount is: "+average);
+    }
+}
